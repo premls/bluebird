ae741a6deb97c0a20c5198723717b00c44594fd9
prem <prem.2007k@gmail.com>
2015-06-05 20:28:25 +0530

    swagger added

---
+++ src/main/java/com/bluebird/config/ApplicationProperties.java
@@ -0,0 +1,17 @@
+package com.bluebird.config;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class ApplicationProperties {
+
+	@Bean
+	public PropertyPlaceholderConfigurer cachePropertiesConfigurer() {
+		PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
+		configurer.setLocations(new Resource[] {new ClassPathResource("jpa.properties"), new ClassPathResource("swagger.properties")});
+		configurer.setIgnoreUnresolvablePlaceholders(true);
+		return configurer;
+	}
+}
